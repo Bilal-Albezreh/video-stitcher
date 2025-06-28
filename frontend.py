b66a6ba4dd1e24571911f834377f90ffc0cb1e6d
Bilal Albezreh <151856347+Bilal-Albezreh@users.noreply.github.com>
2025-06-28 15:03:50 -0400

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -113,6 +113,11 @@ def render_header_and_css(bg_file, logo_file):
                 padding: 0.3rem 0.7rem;
             }}
         }}
+        span[data-testid="stFileUploaderFilename"] {
+            color: #fff !important;
+            font-weight: 600;
+            font-size: 1.1rem;
+        }
         </style>
         <div class="app-header">
             <div class="app-logo"></div>
